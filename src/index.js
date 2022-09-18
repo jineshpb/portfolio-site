11cf3a7b1fd87a04f92cd83cd0568d6692e14a68
jineshpb <jineshpb@gmail.com>
2022-09-18 23:54:55 +0530

    facebook metadata

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB8SJkg_IlopKmqTeYo_oEa21MGjy3lp_8",
+    authDomain: "portfolio-jinesh.firebaseapp.com",
+    projectId: "portfolio-jinesh",
+    storageBucket: "portfolio-jinesh.appspot.com",
+    messagingSenderId: "584684803963",
+    appId: "1:584684803963:web:a10f70ab7cce12a7c50fde",
+    measurementId: "G-68JEWKNNLL"
+  };
+
+  initializeApp(firebaseConfig)
